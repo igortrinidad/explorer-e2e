b26d6e5eacf5e7e0f34ddb96f6222ee97194c460
Igor Trindade <igortrindademe@gmail.com>
2023-01-19 21:19:14 -0300

    test: User login and logout

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "i8mdva",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-localstorage-commands/plugin")(on, config);
+      return config;
     },
   },
 });
--- cypress/e2e/1-auth/login.cy.js
+++ cypress/e2e/1-auth/login.cy.js
@@ -18,30 +18,44 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 // https://on.cypress.io/introduction-to-cypress
 
 describe('Explorer user login', () => {
-  beforeEach(() => {
+
+
+  /**
+   * Select a jurisdiction
+   */
+  before(() => {
     cy.visit('http://localhost:8080')
     cy.get('[data-ref-id="jurisdiction-select-input-home"]').type('Alameda')
     cy.get('#jurisdiction-list-item-0').contains('Alameda')
     cy.get('#jurisdiction-list-item-0').click()
+    cy.saveLocalStorage()
+  })
+  
+  beforeEach(() => {
+    cy.restoreLocalStorage()
+    cy.visit('http://localhost:8080')
   })
 
-  it('Go to login', () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
-    cy.get('#login-register-sidebar-button').click()
+  afterEach(() => {
+    cy.saveLocalStorage()
+  })
 
+  it('User login', () => {
+    cy.get('#login-register-sidebar-button').click()
     cy.get('#signin-email').type('user@domain.com')
     cy.get('#continue-with-email').click()
     cy.get('#signin-password').type('123456')
     cy.get('#login-button').click()
-
-    // We can go even further and check that the default todos each contain
-    // the correct text. We use the `first` and `last` functions
-    // to get just the first and last matched elements individually,
-    // and then perform an assertion with `should`.
-    // cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
-    // cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+    cy.wait(2000)
+    cy.get('.v-toast__item').contains('Login successful')
+  })
+  
+  
+  it('User logout', () => {
+    cy.wait(1000)
+    cy.get('#sidebar-my-account-button').click()
+    cy.get('#my-account-log-out-button').click()
+    cy.get('.v-toast__item').contains('Logged out!')
   })
 
   // it('can add new todo items', () => {
